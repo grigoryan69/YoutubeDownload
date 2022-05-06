dede256b97c6000d29f4bcda0dee8394592b61d9
grigroyan69 <dave69tv@gmail.com>
2022-05-07 01:04:58 +0400

    main was updated

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
-import sys
-from pytube import YouTube
-import eel
+import os
+from youtuber import YouTube
+import linker
 from pathlib import Path
+downloads_dir = str(os.path.join(Path.home(), "Downloads/Youtube Download"))
 
-downloads_path = str(Path.home() / "Downloads")
-eel.init('web')
+if __name__ == '__main__':
+    linker.init('web')
 
+    @linker.expose
+    def recv_data(search):
+        print(search)
 
-@eel.expose
-def recv_data(search):
-    print(search)
-    link = search
+        link = search
 
-    yt = YouTube(link)
-    print("Title: ", yt.title)
-    print("Number of views: ", yt.views)
-    print("Length of video: ", yt.length)
-    print("Rating of video: ", yt.rating)
-    # Getting the highest resolution possible
-    ys = yt.streams.get_highest_resolution()
+        yt = YouTube(link)
+        print("Title: ", yt.title)
+        print("Number of views: ", yt.views)
+        print("Length of video: ", yt.length)
+        # Getting the highest resolution possible
+        ys = yt.streams.get_highest_resolution()
 
-    # Starting download
-    print("Downloading...")
-    ys.download(downloads_path)
-    print("Download completed!")
+        # Starting download
+        print("Downloading...")
+        ys.download(downloads_dir)
+        print("Download completed!")
 
-
-eel.start('main.html', size=(440, 300))
+    linker.start('index.html', size=(440, 300))
