ab1822035e75cc8ee82c5d9c72cdee3c65a8916d
grigroyan69 <dave69tv@gmail.com>
2022-05-07 19:29:06 +0400

    a

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -1,16 +1,4 @@
-
 let btn = document.querySelector('.btn');
-//let download = document.querySelector('#download');
-//btn.addEventListener("click", () => {
-////    document.getElementById("download").style.display = "block";
-//    async function call(){
-//        let name = document.querySelector('.main_input').value;
-//        await linker.title(name)
-//    }
-//    console.log(call());
-//});
-
-
 btn.addEventListener("click", sendData);
 async function sendData() {
     let search = document.querySelector('.main_input').value;
