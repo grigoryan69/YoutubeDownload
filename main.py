a54ef396a330a4d5858551c301c9a58753b83191
grigroyan69 <dave69tv@gmail.com>
2022-05-07 19:21:19 +0400

    added playlist

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import os
+import re
 from youtuber import YouTube
+from youtuber import Playlist
 import linker
 from pathlib import Path
 downloads_dir = str(os.path.join(Path.home(), "Downloads/Youtube Download"))
 
 if __name__ == '__main__':
     linker.init('web')
-
+    
     @linker.expose
-    def recv_data(search):
-        print(search)
+    def content(search):
+        try:
+            playlist = Playlist(search)
+            print(f'Downloading playlist: {playlist.title}')
+            for video in playlist.videos:
+                start = video.streams.get_highest_resolution()
+                start.download(f'{downloads_dir}/{playlist.title}')
 
-        link = search
-
-        yt = YouTube(link)
-        print("Title: ", yt.title)
-        print("Number of views: ", yt.views)
-        print("Length of video: ", yt.length)
-        # Getting the highest resolution possible
-        ys = yt.streams.get_highest_resolution()
-
-        # Starting download
-        print("Downloading...")
-        ys.download(downloads_dir)
-        print("Download completed!")
+            print("Download completed!")
+        
+        except:
+            video = YouTube(search)
+            print("Downloading Video:", video.title)
+            # Getting the highest resolution possible
+            start = video.streams.get_highest_resolution()
+            start.download(downloads_dir)
+            
+            print("Download completed!")
+            
 
     linker.start('index.html', size=(440, 300))
--- web/javascript/script.js
+++ web/javascript/script.js
@@ -1,6 +1,18 @@
+
 let btn = document.querySelector('.btn');
+//let download = document.querySelector('#download');
+//btn.addEventListener("click", () => {
+////    document.getElementById("download").style.display = "block";
+//    async function call(){
+//        let name = document.querySelector('.main_input').value;
+//        await linker.title(name)
+//    }
+//    console.log(call());
+//});
+
+
 btn.addEventListener("click", sendData);
 async function sendData() {
-	let search = document.querySelector('.main_input').value;
-	await linker.recv_data(search);
+    let search = document.querySelector('.main_input').value;
+    await linker.content(search);
 }
